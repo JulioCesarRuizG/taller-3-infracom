1aa61fc673a55f0257314e7a314a26e6d643d736
David Sequera <davidsequerads@gmai.com>
2022-03-20 14:37:29 -0500

    Separate Server and Client

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,34 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		Scanner sc = new Scanner(System.in);
+		String tipo = "";
+		while(!tipo.equals("servidor") && !tipo.equals("cliente"))
+		{
+			System.out.println("Ingrese el tipo de servicio que desea ejecutar(cliente o servidor) :");
+			tipo = sc.nextLine();
+		}
 		System.out.println("Ingrese la cantidad de clientes:");
 		int clientes = sc.nextInt();
-		int archivo = 0;
-		while(archivo != 100 && archivo != 250)
-		{
-			System.out.println("Ingrese el archivo a usar (100 o 250):");
-			archivo = sc.nextInt();
+		if(tipo.equals("servidor")){
+			int archivo = 0;
+			while(archivo != 100 && archivo != 250)
+			{
+				System.out.println("Ingrese el archivo a usar (100 o 250):");
+				archivo = sc.nextInt();
+			}
+			CyclicBarrier barrera = new CyclicBarrier(clientes);
+			new Servidor(barrera, archivo, clientes).start();
+		}else{
+			for(int j=0; j<clientes ; j++)
+			{
+				// Thread.sleep(200);
+				new Cliente(j);
+			}
 		}
-        CyclicBarrier barrera = new CyclicBarrier(clientes);
-		new Servidor(barrera, archivo, clientes).start();
+
+
 		
-		for(int j=0; j<clientes ; j++)
-		{
-			// Thread.sleep(200);
-			new Cliente(j);
-		}
+
 		sc.close();
 	}
 }
